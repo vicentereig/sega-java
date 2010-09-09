06d3b03cf34485663e517053e46a341bd2a57942
Vicente Reig <vicente.reig@gmail.com>
2010-09-09 11:41:04 +0200

    pushing new files (object oriented xpath lexer)

---
+++ src/main/java/com/google/code/sega/lexer/Scanner.java
@@ -0,0 +1,11 @@
+package com.google.code.sega.lexer;
+
+public interface Scanner {
+
+	public boolean matches(int c);
+
+	public void addToLexeme(int c);
+
+	public Token buildToken();
+
+}
---
+++ src/main/java/com/google/code/sega/lexer/TokenListener.java
@@ -0,0 +1,5 @@
+package com.google.code.sega.lexer;
+
+public interface TokenListener {
+	public void tokenRecognised(Token t);
+}
